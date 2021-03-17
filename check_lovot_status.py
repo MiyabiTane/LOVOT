8d19b2f1875456077f962f67c8c5041a8fe0884b
MiyabiTane <miyabitane@yahoo.co.jp>
2021-03-17 18:38:08 +0900

    update

--- check_lovot_status.py
+++ check_lovot_status.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import copy
 import time
+from copy import deepcopy
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -21,10 +22,12 @@ class CheckStatus:
         self.nest_img = cv2.imread(NEST_PATH)
         self.lamp_img = cv2.imread(LAMP_PATH)
         self.horn_img = cv2.imread(HORN_PATH)
-        self.TH = 0.6
+        self.TH = 0.7
         self.status = -1
         self.tm = time.time()
 
+        self.debug_view = rospy.get_param('~debug_view', False)
+        
         rospy.Subscriber("/usb_cam/image_raw", Image, self.img_cb)
 
 
@@ -43,18 +46,24 @@ class CheckStatus:
         res = cv2.matchTemplate(input_img, self.nest_img, cv2.TM_CCOEFF_NORMED)
         _min_val, _max_val, _min_loc, max_loc = cv2.minMaxLoc(res)
         top_left = max_loc
-        # bottom_right = (top_left[0] + W, top_left[1] + H)
-        # cv2.rectangle(img, top_left, bottom_right, (255, 0, 0), thickness=2, lineType=cv2.LINE_4)
 
         monitor_lt = (top_left[0] + 5, top_left[1] + H - 10)
         monitor_rb = (monitor_lt[0] + 65, monitor_lt[1] + 60)
         monitor_img = self.img[monitor_lt[1]: monitor_rb[1], monitor_lt[0]: monitor_rb[0], :]
-        # cv2.rectangle(img, monitor_lt, monitor_rb, (0, 255, 0), thickness=2, lineType=cv2.LINE_4)
 
         panel_lt = (top_left[0] - 90, top_left[1] - 20)
         panel_rb = (panel_lt[0] + 100, panel_lt[1] + 60)
         panel_img = self.img[panel_lt[1]: panel_rb[1], panel_lt[0]: panel_rb[0], :]
-        # cv2.rectangle(img, panel_lt, panel_rb, (0, 0, 255), thickness=2, lineType=cv2.LINE_4)
+
+        if self.debug_view:
+            output_img = deepcopy(self.img)
+            bottom_right = (top_left[0] + W, top_left[1] + H)
+            cv2.rectangle(output_img, top_left, bottom_right, (255, 0, 0), thickness=2, lineType=cv2.LINE_4)
+        
+            cv2.rectangle(output_img, monitor_lt, monitor_rb, (0, 255, 0), thickness=2, lineType=cv2.LINE_4)
+        
+            cv2.rectangle(output_img, panel_lt, panel_rb, (0, 0, 255), thickness=2, lineType=cv2.LINE_4)
+            cv2.imwrite("debug.png", output_img)
 
         return monitor_img, panel_img
 
@@ -71,14 +80,27 @@ class CheckStatus:
         lamp_status = False
 
         res1 = cv2.matchTemplate(panel_img, self.horn_img, cv2.TM_CCOEFF_NORMED)
-        loc1 = np.where(res1 > self.TH)
-        if len(loc1) > 5:
+        _min_val, max_val_1, _min_loc, max_loc_1 = cv2.minMaxLoc(res1)
+        if max_val_1 > self.TH:
             horn_status = True
 
         res2 = cv2.matchTemplate(monitor_img, self.lamp_img, cv2.TM_CCOEFF_NORMED)
-        loc2 = np.where(res2 > self.TH)
-        if len(loc2) > 5:
+        _min_val, max_val_2, _min_loc, max_loc_2 = cv2.minMaxLoc(res2)
+        if max_val_2 > self.TH:
             lamp_status = True
+
+        if self.debug_view:
+            H, W, _C = self.horn_img.shape
+            top_left = max_loc_1
+            bottom_right = (top_left[0] + W, top_left[1] + H)
+            cv2.rectangle(panel_img, top_left, bottom_right, (255, 0, 0), thickness=2, lineType=cv2.LINE_4)
+            cv2.imwrite("debug_horn.png", panel_img)
+
+            H, W, _C = self.lamp_img.shape
+            top_left = max_loc_2
+            bottom_right = (top_left[0] + W, top_left[1] + H)
+            cv2.rectangle(monitor_img, top_left, bottom_right, (255, 0, 0), thickness=2, lineType=cv2.LINE_4)
+            cv2.imwrite("debug_lamp.png", monitor_img)
 
         if not horn_status:  # LOVOTが充電場所にいない
             if not lamp_status:
@@ -93,11 +115,10 @@ class CheckStatus:
 
 
     def img_cb(self, msg):
-        self.img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="rgb8")
+        self.img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         monitor_img, panel_img = self.search_range(self.img)
         status = self.check_status(monitor_img, panel_img)
-        print(status)
-
+        
 
 if __name__ == '__main__':
     rospy.init_node("LOVOT")
